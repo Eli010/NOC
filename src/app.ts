404b0badabb22b662d704f130e62daa50259eb2f
elíPacombia <pacombiaeli@gmail.com>
2024-01-01 07:41:35 -0500

    crear y traer mis datos de mongo

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import { Server } from "./presentation/server";
 import { envs } from './config/plugins/envs.plugin';
-import { MongoDataBase } from './data/mongo';
+import { LogModel, MongoDataBase } from './data/mongo';
 
 
 //función anonima auto-invocable
@@ -16,9 +16,22 @@ async function main(){
         dbName:envs.MONGO_DB_NAME,
     })
 
+    //Crear una colección = tabla, documento = row
+    // const newLog = await LogModel.create({
+    //     message:'Test message desde mongo',
+    //     origin:'App.ts',
+    //     level:'low'
+    // });
+    // //guardamos los datos en nuestro db
+    // await newLog.save();
+    // console.log(newLog);
+    
+    //traer todos los datos
+    const logs = await LogModel.find();
+    console.log(logs);
+    
+
     //Server.start();
-    // console.log(process.env.MAILER_EMAIL);
-    // console.log(envs);
     
 }
 
