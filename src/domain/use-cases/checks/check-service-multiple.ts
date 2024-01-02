e4cf9fb504ed35e9ba72e646d654f65e840f7e17
elíPacombia <pacombiaeli@gmail.com>
2024-01-02 07:41:02 -0500

    use multiple db

---
+++ src/domain/use-cases/checks/check-service-multiple.ts
@@ -0,0 +1,61 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
+
+interface CheckServiceMultipleUseCase{
+    execute( url:string ):Promise<boolean>;
+}
+//1.- la información que vamos a inyectar
+type SuccessCallback = (() =>void) | undefined;
+type ErrorCallback =((error:string) => void) | undefined;
+
+export class CheckServiceMultiple implements CheckServiceMultipleUseCase {
+
+    //2.- INYECCION DE DEPENDENCIA #1 para ver si falla mi conexion o no
+    constructor(
+        private readonly logRepository:LogRepository[],//<--
+        private readonly successCallback :SuccessCallback,
+        private readonly errorCallback :ErrorCallback
+    ){}
+
+    private callLog(log:LogEntity){
+        this.logRepository.forEach(logRepository =>{
+            logRepository.saveLog(log)
+        })
+    }
+
+    public async execute(url: string): Promise<boolean> {
+        try {
+            //conexion al servidor
+            const req = await fetch(url);
+            if (!req.ok) {
+                throw new Error(`Error on check service ${url}`);
+            }
+            const log = new LogEntity({
+                message:`Service ${url} working`,
+                level:LogSeverityLevel.low,
+                origin:'check-service.ts'});
+            // this.logRepository.saveLog(log);
+            this.callLog(log);
+            //3.- Si todo esta bien llamamos a nuestro successCallback
+            this.successCallback && this.successCallback();
+            console.log(`${url} is ok`);
+            
+            return true;
+        } catch (error) {
+            // console.log(`${error}`);
+            const errorMessage = `${url} is not ok. ${error}`;
+            const log = new LogEntity({
+                message:errorMessage,
+                level:LogSeverityLevel.high,
+                origin:'check-service.ts',
+            });
+            // this.logRepository.saveLog(log);
+            this.callLog(log);
+            
+            //3.- Si hay errorres llamamo a nuestro errorCallback
+            this.errorCallback && this.errorCallback(`${error}`);
+            return false;
+        }
+    }
+
+}
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,7 @@
 import { envs } from "../config/plugins/envs.plugin";
 import { LogSeverityLevel } from "../domain/entities/log.entity";
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CheckServiceMultiple } from "../domain/use-cases/checks/check-service-multiple";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDatasource } from "../infraestructure/datasources/file-system.datasource";
 import { MongoLogDatasource } from "../infraestructure/datasources/mongo-log.datasource";
@@ -11,35 +12,61 @@ import { EmailService } from "./email/email-service";
 
 
 //instancias de las implementaciones, file systen datasource
-const logRepository = new LogRepositoryImpl(
-    // new FileSystemDatasource(),
-    // new MongoLogDatasource(),
-    new PostgresLogDatasource()
-    // new OracleLogDs()
+// const logRepository = new LogRepositoryImpl(
+//     // new FileSystemDatasource(),
+//     // new MongoLogDatasource(),
+//     new PostgresLogDatasource()
+// );
+//instacias de nuestros logs repositories multiples
+const fsLogRepository = new LogRepositoryImpl(
+    new FileSystemDatasource()
 );
+const mongoLogRepository = new LogRepositoryImpl(
+    new MongoLogDatasource(),
+);
+const postgresLogRepository = new LogRepositoryImpl(
+    new PostgresLogDatasource(),
+);
+
+
+
 const emailService = new EmailService();
 
-export class Server{
-    public static async start(){
+export class Server {
+    public static async start() {
         console.log('Server started...');
-        
+
         // const logs = await logRepository.getLogs(LogSeverityLevel.low);
         // console.log(logs);
-        
+
 
         //invocamos nuestra class cron server
         CronServer.createJob(
             //agremos el valor de mi tiempo  de ejecución
             '*/5 * * * * *',
             //agrego que tendra en mi función
-            ()=>{
-                const url ='https://google.com'
-                new CheckService(
-                    logRepository,
-                    ()=> console.log(`${url} is ok`),
-                    (error)=>console.log(error)
+            () => {
+                const url = 'https://google.com'
+                new CheckServiceMultiple(
+                    [fsLogRepository,postgresLogRepository,mongoLogRepository],
+                    () => console.log(`${url} is ok`),
+                    (error) => console.log(error)
                 ).execute(url);
             }
-        );        
+        );
+        // //invocamos nuestra class cron server
+        // CronServer.createJob(
+        //     //agremos el valor de mi tiempo  de ejecución
+        //     '*/5 * * * * *',
+        //     //agrego que tendra en mi función
+        //     ()=>{
+        //         const url ='https://google.com'
+        //         new CheckService(
+        //             logRepository,
+        //             ()=> console.log(`${url} is ok`),
+        //             (error)=>console.log(error)
+        //         ).execute(url);
+        //     }
+        // );        
     }
 }
